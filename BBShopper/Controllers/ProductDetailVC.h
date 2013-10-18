3678714cc8a94274963fb9bc74993a0ce7f8344d
Mike Frengel <mfrengel@truefitinnovation.com>
2013-10-17 23:49:17 -0400

    Added ProductDetailVC view and controller files

---
+++ BBShopper/Controllers/ProductDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetailVC.h
+//  BBShopper
+//
+//  Created by MFrengel on 10/17/13.
+//  Copyright (c) 2013 MFrengel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductDetailVC : UIViewController
+
+@end
