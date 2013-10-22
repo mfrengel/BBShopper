dde6428316f020f4a5dea0c7c5ab59b3c99e6bc3
Mike Frengel <mfrengel@truefitinnovation.com>
2013-10-22 18:16:02 -0400

    Implemented getProductDetail API to fetch product variations on viewDidLoad

--- BBShopper/Controllers/ProductDetailVC.m
+++ BBShopper/Controllers/ProductDetailVC.m
@@ -9,8 +9,12 @@
 #import "ProductDetailVC.h"
 #import "UIImageView+AFNetworking.h"
 #import "CartVC.h"
+#import "APIProductService.h"
 
-@interface ProductDetailVC ()
+@interface ProductDetailVC()<APIProductServiceDelegate>
+
+@property (strong) APIProductService* productAPI;
+@property (strong) ProductDetail* productDetail;
 
 @end
 
@@ -30,14 +34,15 @@
     NSString* price = [self.product.price valueForKey:@"value"];
     self.prodPrice.text = [formatter stringFromNumber: [NSNumber numberWithDouble:[price doubleValue]]];
     
-    NSLog(@"href: %@", self.product.href);
-    
     // Just show the 1st promo
     if (self.product.promos.count > 0) {
         self.prodPromo.text = ((ProductPromo*)[self.product.promos objectAtIndex:0]).description;
     }
     else
-        self.prodPromo.text = @"There are no promotions at this time, bummer dude.";
+        self.prodPromo.text = @"There are no promotions at this time.";
+    
+    self.productAPI.delegate = self;
+    [self.productAPI getProductDetail:self.product];
 }
 
 
@@ -49,11 +54,30 @@
     // 2. Response to delegate success/fail message
     // 3. Upon success, show the CartVC
     
-    [self AddToCartSuccess];
+    [self addToCartSuccess];
+}
+
+#pragma mark - APIProductServiceDelegate
+-(void) getProductDetailSuccess:(ProductDetail*)productDetail {
+    self.productDetail = productDetail;
+    
+    NSLog(@"%i variations", productDetail.variations.count);
+    
+    for (ProductVariation* v in productDetail.variations) {
+        NSLog(@"\nMethod: %@, action: %@, id: %@", v.availability.online.forms.add_to_cart.method, v.availability.online.forms.add_to_cart.action, [v.availability.online.forms.add_to_cart.inputs objectForKey:@"id"]);
+    }
+}
+
+-(void) getProductDetailFailed:(NSString *)error {
+    [[[UIAlertView alloc] initWithTitle:@"Load Failed"
+                                message:error
+                               delegate:nil
+                      cancelButtonTitle:@"Ok"
+                      otherButtonTitles:nil] show];
 }
 
 #pragma mark - Cart delegate
--(void) AddToCartSuccess {
+-(void) addToCartSuccess {
     CartVC* vc = [CartVC object];
     [self.navigationController pushViewController:vc animated:YES];
     
