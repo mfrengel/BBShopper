0d18b1c62507b95c266e24ec7e95621070c6c563
Mike Frengel <mfrengel@truefitinnovation.com>
2013-10-23 17:39:26 -0400

    clean up addProductToCart: withVariation method

--- BBShopper/Services/Web/APICartService.m
+++ BBShopper/Services/Web/APICartService.m
@@ -14,34 +14,13 @@
 -(void) addProductToCart:(Product*)product withVariation:(ProductVariation*)variation {
     // TODO: check internet connection, notify of failure if no connection & return
     
-    BBAPIClient* client = [BBAPIClient sharedClient];
-    [client setParameterEncoding:AFFormURLParameterEncoding];
-
-    NSString* formValue = [NSString stringWithFormat:@"id=%@&qty=1", [variation.availability.online.forms.add_to_cart.inputs objectForKey:@"id"]];
-    formValue = [formValue stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
-  
-    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL: [NSURL URLWithString: variation.availability.online.forms.add_to_cart.action relativeToURL: client.baseURL]];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL: [NSURL URLWithString: variation.availability.online.forms.add_to_cart.action relativeToURL: [BBAPIClient sharedClient].baseURL]];
     [request setHTTPMethod:@"POST"];
     [request setValue: @"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    NSString* formValue = [NSString stringWithFormat:@"id=%@&qty=1", [variation.availability.online.forms.add_to_cart.inputs objectForKey:@"id"]];
     [request setHTTPBody: [formValue dataUsingEncoding:NSUTF8StringEncoding]];
     
     BBHTTPRequestOperation *operation = [[BBHTTPRequestOperation alloc] initWithRequest:request];
-//    [operation setRedirectResponseBlock:^NSURLRequest *(NSURLConnection *connection, NSURLRequest *request, NSURLResponse *redirectResponse) {
-//        if (redirectResponse) {
-//            NSLog(@"Redirect");
-//            
-//            NSMutableURLRequest *r = [request mutableCopy]; // original request
-//            [r setURL: [request URL]];
-//            
-//            NSLog(@"redirect headers: %@, url: %@, method: %@", r.allHTTPHeaderFields, r.URL, r.HTTPMethod);
-//
-//            return r;
-//        }
-//        else {
-//            return request;
-//        }
-//    }];
-    
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"Response: %@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
        
